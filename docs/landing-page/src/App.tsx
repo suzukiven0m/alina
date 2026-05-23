acc73fcaec727c80740937f81283df9008e394ce
suzukiven0m <facundo.fanti@gmail.com>
2026-05-23 17:32:31 +0200

    feat: rewire App and remove unused visual components

--- docs/landing-page/src/App.tsx
+++ docs/landing-page/src/App.tsx
@@ -1,23 +1,19 @@
-import Hero from './sections/Hero'
+import Header from './sections/Header'
+import SystemOverview from './sections/SystemOverview'
 import Architecture from './sections/Architecture'
 import Challenges from './sections/Challenges'
 import TechStack from './sections/TechStack'
-import LiveMetrics from './sections/LiveMetrics'
-import CodeHighlights from './sections/CodeHighlights'
-import Demo from './sections/Demo'
 import GettingStarted from './sections/GettingStarted'
 import Footer from './sections/Footer'
 
 export default function App() {
   return (
-    <div className="relative min-h-screen">
-      <Hero />
+    <div className="min-h-screen">
+      <Header />
+      <SystemOverview />
       <Architecture />
       <Challenges />
       <TechStack />
-      <LiveMetrics />
-      <CodeHighlights />
-      <Demo />
       <GettingStarted />
       <Footer />
     </div>
